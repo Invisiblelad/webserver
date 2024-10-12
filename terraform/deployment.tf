0b5dee34b9d71f6040570dbb2966d26078446a1f
Sharath Veerapaneni <140253698+Invisiblelad@users.noreply.github.com>
2024-10-12 11:41:50 +0530

    Update deployment.tf

--- terraform/deployment.tf
+++ terraform/deployment.tf
@@ -4,7 +4,7 @@ resource "kubernetes_deployment" "webpage" {
       namespace = "default"
     }
     spec {
-      replicas = 2
+      replicas = 3
       selector {
         match_labels = {
           app = "webpage"
